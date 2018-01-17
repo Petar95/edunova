1c2998872431965921302afde427b3042f297e65
Petar <33255362+Petar95@users.noreply.github.com>
2018-01-17 16:20:39 +0100

    Add files via upload

---
+++ ZimskiZadatak/src/ZimskiZadatak/Zadatak4.java
@@ -0,0 +1,21 @@
+package ZimskiZadatak;
+
+public class Zadatak4 {
+
+	public static void main(String[] args) {
+		int a[][] = { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+		int b[][] = { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+
+		int c[][] = new int[4][4];
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				c[i][j] = a[i][j] + b[i][j];
+				System.out.print(c[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
